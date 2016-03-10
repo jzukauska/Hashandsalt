4b56195c440bc11de43f4e85f09a69c427abefe6
jzukauska <j.zukauska@gmail.com>
2016-03-10 12:29:40 -0600

    Second page gui working.

--- tabs_app/src/main/java/com/example/mytabs/tabs_app/SecondFragment.java
+++ tabs_app/src/main/java/com/example/mytabs/tabs_app/SecondFragment.java
@@ -2,6 +2,7 @@ package com.example.mytabs.tabs_app;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -31,7 +32,7 @@ public class SecondFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View fragView = inflater.inflate(R.layout.fragament_two, container, false);
+        final View fragView = inflater.inflate(R.layout.fragament_two, container, false);
 
          text = (EditText) fragView.findViewById(R.id.editText2);
 
@@ -40,14 +41,37 @@ public class SecondFragment extends Fragment {
         populateList(saltList, fragView);
 
 
-        ArrayAdapter<String> dynamicArrayHolder =
+        final ArrayAdapter<String> dynamicArrayHolder =
                 new ArrayAdapter<String>(getActivity(),
                         android.R.layout.simple_spinner_dropdown_item,
                         saltList);
 
         spinz = (Spinner) fragView.findViewById(R.id.spinner);//Associate "spinz" with xml
         spinz.setAdapter(dynamicArrayHolder);//
+
+        text.setOnKeyListener(new View.OnKeyListener() {
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+                if (event.getAction() == android.view.KeyEvent.ACTION_DOWN) {
+                    switch (keyCode) {
+
+                        case android.view.KeyEvent.KEYCODE_DPAD_CENTER:
+                        case android.view.KeyEvent.KEYCODE_ENTER:
+                            populateList(saltList, fragView);
+
+                            return true;
+                        default:
+                            break;
+                    }
+                }
+                return false;
+            }
+        });
+
+
+
         return fragView;
+
+
     }
 
 
@@ -59,6 +83,8 @@ public class SecondFragment extends Fragment {
     }
 
     public void populateList(ArrayList list, View v) {
+
+        list.clear();
         for (int i = 0; i < getLength(v); i++) {
 
             list.add(Integer.toString(i + 1));
@@ -67,4 +93,5 @@ public class SecondFragment extends Fragment {
 
 
     }
+
 }
